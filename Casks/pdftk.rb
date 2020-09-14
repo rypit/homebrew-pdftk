334db3545055e2cf9499aaf4ae72afba54f0bc2a
RJ Pittman <rypitme@gmail.com>
2020-09-14 15:20:19 -0400

    Address stylistic warnings

--- Casks/pdftk.rb
+++ Casks/pdftk.rb
@@ -1,10 +1,15 @@
+# frozen_string_literal: true
+
 cask "pdftk" do
-  VERSION   = '2.02'
-  homepage  'http://www.pdflabs.com/tools/pdftk-server'
-  url       "https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-#{VERSION}-mac_osx-10.11-setup.pkg"
-  sha256    'c33cf95151e477953cd57c1ea9c99ebdc29d75f4c9af0d5f947b385995750b0c'
+  VERSION   = "2.02"
   version   VERSION
-  name      'PDFtk Server - The PDF Toolkit for the Terminal'
+  sha256    "c33cf95151e477953cd57c1ea9c99ebdc29d75f4c9af0d5f947b385995750b0c"
+
+  url       "https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-#{VERSION}-mac_osx-10.11-setup.pkg"
+  name      "PDFtk Server - The PDF Toolkit for the Terminal"
+  homepage  "http://www.pdflabs.com/tools/pdftk-server"
+
   pkg       "pdftk_server-#{VERSION}-mac_osx-10.11-setup.pkg"
-  uninstall pkgutil: 'com.pdflabs.pdftkThePdfToolkit.pdftk.pkg'
+
+  uninstall pkgutil: "com.pdflabs.pdftkThePdfToolkit.pdftk.pkg"
 end
